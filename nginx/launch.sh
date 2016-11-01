198a72d86495908dd64a24c5685c2e733b7987ef
Zach Bintliff <zach.bintliff@mlb.com>
2016-11-01 12:04:10 -0400

    update to work

--- nginx/launch.sh
+++ nginx/launch.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-nginx
-
 /usr/local/bin/consul-template -config /etc/consul-template/config.d \
                                -wait 2s:10s \
-                               -consul consul:8500
+                               -consul consul:8500 \
+                               -exec "nginx"
